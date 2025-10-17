73712234df7a38f29850323b309f542a47cb193c
osvaldo <osvaldoramos681@gmail.com>
2025-10-17 15:35:48 -0600

    second commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="items-center justify-center flex-1 bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!!
+      </Text>
+    </SafeAreaView>
+  );
+}
